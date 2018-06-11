2501cc9e58eada5fed77981233a885673faa51a4
al_architjain <architjain.delhi2@gmail.com>
2018-06-11 12:03:28 +0530

    Authentication Defined

--- SORP/sorp_app/urls.py
+++ SORP/sorp_app/urls.py
@@ -4,7 +4,8 @@ from django.conf.urls import url
 from . import views 
 
 urlpatterns=[
-      url(r'^$',views.user_login_page,name='user_login_page'),
+      url(r'^$',views.user_login,name='user_login_page'),
+      url(r'^profile/$', views.user_profile, name = 'user_profile_page')
       #url(r'^verify/',views.checking,name='checking')
   ]
 
--- SORP/sorp_app/views.py
+++ SORP/sorp_app/views.py
@@ -1,18 +1,26 @@
 from django.shortcuts import render
 from django.http import *
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User, Group
 
 #imports form sorp_app
 from . import forms
 
+#function to find the group of user
+def get_user_group(user):
+	g_name = user.groups.values_list('name',flat=True)
+	return g_name
+
+
 #render login page
-def user_login_page(request) :
+def user_login(request) :
 	form = forms.login_form()
 
 	if request.method == "POST":
 		form = forms.login_form(request.POST)
+
+		username = password = ''
 
 		if form.is_valid():
 			username = request.POST['username']
@@ -21,11 +29,20 @@ def user_login_page(request) :
 			user = authenticate(request, username=username, password=password)
 
 			if user is not None:
-				return HttpResponse("User Exists :)")
+				login(request, user)
+				return HttpResponseRedirect('profile/')
 			else:
-				return HttpResponse("User doesnot exists")
+				return HttpResponseRedirect('/login/')
 
 	return render(request,'sorp_app/login.html', {'form':form})
+
+
+
+#user_profile
+def user_profile(request):
+	return HttpResponse("<h2>You are logged IN!</h2>")
+
+
 
 
 #authenticate and check who the user is!
