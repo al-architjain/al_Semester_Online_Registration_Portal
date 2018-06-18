d968fa03fd4f8b346d9f6929b61006f5f89f7bdd
al_architjain <architjain.delhi2@gmail.com>
2018-06-18 19:54:29 +0530

    Final Models created

--- SORP/sorp_app/models.py
+++ SORP/sorp_app/models.py
@@ -2,63 +2,87 @@ from django.db import models
 from django.contrib.auth.models import User
 from decimal import Decimal
 
-import datetime
 
-current_date = datetime.datetime.now()
+from datetime import datetime
+## Function to calculate current year :)
+def currYear():
+    now = datetime.now()
+    return now.year
+##
+
 
 class Board(models.Model):
+    id = models.PositiveIntegerField(unique=True)
     name = models.CharField(primary_key=True, max_length=12)
-    id = models.IntegerField(unique=True)
     full_name = models.CharField(max_length=100)
 
 class Category(models.Model):
+    id = models.PositiveIntegerField(unique=True)
     name = models.CharField(primary_key=True, max_length=12)
-    id = models.IntegerField(unique=True)
-    full_name = models.CharField(max_length=100)
+    full_name = models.CharField(max_length=60)
 
 class UGClass(models.Model):
-    id = models.IntegerField(unique=True)
+    id = models.PositiveIntegerField(unique=True)
     name = models.CharField(primary_key=True, max_length=25)
-    full_name = models.CharField(max_length=100)
+    full_name = models.CharField(max_length=60)
 
 class UGBranch(models.Model):
     name = models.CharField(primary_key=True, max_length=30)
-    full_name = models.CharField(max_length=400)
+    full_name = models.CharField(max_length=100)
     code = models.CharField(max_length=10)
     section = models.CharField(max_length=5)
 
 class Subjects(models.Model):
     classname = models.ForeignKey(UGClass, on_delete=models.CASCADE)
     branch = models.ForeignKey(UGBranch, on_delete=models.CASCADE)
-    semester = models.IntegerField()
-    subject_name = models.CharField(max_length=200)
-    subject_code = models.CharField(max_length=20)
-    year = models.IntegerField()
+    semester_choice = (
+        (1,1),
+        (2,2),
+        (3,3),
+        (4,4),
+        (5,5),
+        (6,6),
+        (7,7),
+        (8,8),
+        (9,9),
+        (10,10),
+    )
+    semester = models.IntegerField(choices=semester_choice)
+    sub_code = models.CharField(primary_key=True,max_length=20)
+    sub_name = models.CharField(max_length=200)
 
 
-class StudentBasicInfo(models.Model):
-    user_id = models.OneToOneField(
+class Documents(models.Model):
+    doc_name = models.CharField(max_length=500)
+    doc_imp = models.CharField(max_length=2, default = 'M', choices=(('M', 'Mandatory'), ('O', 'Optional'),('NA', 'Not Applicable')))
+
+
+class StudentInfo(models.Model):
+    user = models.OneToOneField(
         User, on_delete=models.PROTECT, default=0
     )
     #
+    # Documents
+    stud_doc = models.ManyToManyField(Documents, through='DocumentInfo')
+    #
     # Personal Details
-    student_name_eng = models.CharField(max_length=60)
-    student_name_hindi = models.CharField(max_length=60, null=True)
+    name_eng = models.CharField(max_length=60)
+    name_hindi = models.CharField(max_length=60, null=True)
     email = models.EmailField()
     gender_choices = (
         ('Male', 'Male'),
         ('Female', 'Female'),
-        ('Other', 'other')
+        ('Other', 'Other')
     )
     gender = models.CharField(max_length=10, choices=gender_choices)
-    date_of_birth = models.DateField(max_length=8)
-    religion = models.CharField(max_length=30)
-    main_category = models.ForeignKey(Category, on_delete=models.PROTECT)
-    student_contact = models.IntegerField(null=True)
-    aadhar_no = models.IntegerField(unique='True', null=True)
+    dob = models.DateField()
+    religion = models.CharField(max_length=20)
+    category_main = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='main')
+    contact = models.CharField(max_length=20, null=True)
+    aadhar_no = models.CharField(max_length=16, unique='True', null=True)
     area_choice = (
         ('Rural', 'Rural'),
-        ('Urban', 'Urban')
+        ('Urban', 'Urban'),
     )
     area = models.CharField(max_length=10, choices=area_choice)
     bonafide_country = models.CharField(max_length=30)
@@ -68,16 +92,16 @@ class StudentBasicInfo(models.Model):
     permanent_add = models.CharField(max_length=1000)
     #
     # Academics Details
-    year_of_admission = models.IntegerField()
+    year_of_admission = models.IntegerField(default=currYear)
     jee_roll_no = models.BigIntegerField()
-    jee_score = models.IntegerField()
-    jee_AI_rank = models.IntegerField()
-    jee_category_rank = models.IntegerField()
-    admission_category = models.CharField(max_length=10)
-    intermediate_pass_country = models.CharField(max_length=30)
-    intermediate_pass_state = models.CharField(max_length=30)
+    jee_score = models.PositiveIntegerField()
+    jee_AI_rank = models.PositiveIntegerField()
+    jee_category_rank = models.PositiveIntegerField(null=True)
+    category_admission = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='admission')
+    intermediate_country = models.CharField(max_length=30)
+    intermediate_state = models.CharField(max_length=30)
     intermediate_percentage = models.DecimalField(max_digits=5, decimal_places=3)
-    intermediate_pass_year = models.CharField(max_length=4)
+    intermediate_pass_year = models.IntegerField(default=currYear)
     school_type_choices = (
         ('Government', 'Government School'),
         ('Private', 'Private School')
@@ -88,7 +112,19 @@ class StudentBasicInfo(models.Model):
     intermediate_school_board = models.ForeignKey(Board, on_delete=models.PROTECT)
     ug_class = models.ForeignKey(UGClass, on_delete=models.PROTECT)
     ug_branch = models.ForeignKey(UGBranch, on_delete=models.PROTECT)
-    # section = models.CharField(max_length=25)
+    semester_choice = (
+        (1, 1),
+        (2, 2),
+        (3, 3),
+        (4, 4),
+        (5, 5),
+        (6, 6),
+        (7, 7),
+        (8, 8),
+        (9, 9),
+        (10, 10),
+    )
+    ug_sem = models.SmallIntegerField(choices=semester_choice, default=1)
     hosteler = models.BooleanField()
     hostel_choices=(
         ('KBH', 'Kailash Boys Hostel'),
@@ -102,35 +138,35 @@ class StudentBasicInfo(models.Model):
         ('AGH', 'Ambika Girls Hostel'),
         ('PGH', 'Parvati Girls Hostel'),
     )
-    hostel_name = models.CharField(max_length=50, choices=hostel_choices)
-    entry_no = models.IntegerField(default=None)
-    reg_no = models.CharField(max_length=50)
+    hostel_name = models.CharField(max_length=50, choices=hostel_choices, null=True)
+    entry_no = models.PositiveIntegerField(unique=True,default=None)
+    reg_no = models.CharField(unique=True,max_length=50)
     roll_no = models.CharField(unique=True, max_length=10)
-    active_status = models.BooleanField(default=True)
+    active_status = models.BooleanField(default=True)   # (1 is active) and (0 is inactive)
     #
     # Guardian Details
     father_name = models.CharField(max_length=60)
-    father_contact = models.IntegerField()
+    father_contact = models.CharField(max_length=20)
     father_email = models.EmailField()
     mother_name = models.CharField(max_length=60)
-    mother_contact = models.IntegerField()
+    mother_contact = models.CharField(max_length=20)
     mother_email = models.EmailField()
     guardian_name = models.CharField(max_length=60)
-    guardian_contact = models.IntegerField()
+    guardian_contact = models.CharField(max_length=20)
     guardian_email = models.EmailField()
-    family_income = models.IntegerField(default=0)
+    family_income = models.PositiveIntegerField(default=0)
     fee_waiver_claim = models.CharField(max_length=60)
 
 class StudentMedicalInfo(models.Model):
     student_id = models.OneToOneField(
-        StudentBasicInfo, on_delete=models.CASCADE, default=0
+        StudentInfo, on_delete=models.CASCADE, default=0
     )
     age = models.IntegerField()
     identification_mark = models.CharField(max_length=40)
     major_illness = models.CharField(max_length=40)
     blood_group = models.CharField(max_length=5)
     vision = models.CharField(max_length=3)
-    height_in_cm = models.IntegerField()
+    height_in_cm = models.SmallIntegerField()
     # weight_kg = models.IntegerField()
     past_mental_illness = models.CharField(max_length=30)
     clour_blindness = models.CharField(max_length=10)
@@ -138,43 +174,44 @@ class StudentMedicalInfo(models.Model):
 
 
 class StudentFirstFeeStatus(models.Model):
-    #
     student_id = models.OneToOneField(
-        StudentBasicInfo, on_delete=models.CASCADE
+        StudentInfo, on_delete=models.CASCADE
     )
-    fee_josaa_amount = models.IntegerField()
+    fee_josaa_amount = models.PositiveIntegerField()
     fee_josaa_date = models.DateField(auto_now=False, auto_now_add=False, default=None)
-    fee_NITH_amount = models.IntegerField()
+    fee_NITH_amount = models.PositiveIntegerField()
     fee_nith_date = models.DateField(auto_now=False, auto_now_add=False, default=None)
     fee_NIT_receipt_no = models.CharField(max_length=25)
-
-
-
-class Result(models.Model):
-    student_id = models.OneToOneField(
-        StudentBasicInfo, on_delete=models.CASCADE
-    )
-    semester = models.IntegerField(),
-    sgpi = models.DecimalField(max_digits=5, decimal_places=3, default=Decimal(0.00))
-    cgpi = models.DecimalField(max_digits=5, decimal_places=3, default=Decimal(0.00))
-
-
-
-class Documents(models.Model):
-    year = models.IntegerField(default=current_date.year)
-    document = models.CharField(max_length=500)
-    stud_doc = models.ManyToManyField(StudentBasicInfo, through='DocumentInfo')
-    mandatory = models.BooleanField(default=True)
+    fee_total = models.PositiveIntegerField()
 
 
 class DocumentInfo(models.Model):
-    stud_id = models.ForeignKey(StudentBasicInfo, on_delete=models.CASCADE)
+    stud_id = models.ForeignKey(StudentInfo, on_delete=models.CASCADE)
     doc_id = models.ForeignKey(Documents, on_delete=models.CASCADE)
     submitted = models.BooleanField(default=False)
 
 
+class Result(models.Model):
+    roll_no = models.OneToOneField(StudentInfo, to_field='roll_no', on_delete=models.CASCADE, unique=True)
+    semester_choice = (
+        (1, 1),
+        (2, 2),
+        (3, 3),
+        (4, 4),
+        (5, 5),
+        (6, 6),
+        (7, 7),
+        (8, 8),
+        (9, 9),
+        (10, 10),
+    )
+    semester = models.SmallIntegerField(choices=semester_choice, default=1)
+    sgpi = models.DecimalField(max_digits=4, decimal_places=2, default=Decimal(0.00))
+    cgpi = models.DecimalField(max_digits=4, decimal_places=2, default=Decimal(0.00))
+
+
 class Due(models.Model):
-    roll_num = models.ForeignKey(StudentBasicInfo,to_field='roll_no',on_delete=models.CASCADE)
-    library_due = models.DecimalField(max_digits=100,decimal_places=4,default=Decimal(0.00))
-    hostel_balance = models.DecimalField(max_digits=100,decimal_places=4,default=Decimal(0.00))
-    acdemic_due = models.DecimalField(max_digits=100,decimal_places=4,default=Decimal(0.00))
+    roll_no = models.OneToOneField(StudentInfo,to_field='roll_no',on_delete=models.CASCADE, unique=True)
+    library_due = models.DecimalField(max_digits=10,decimal_places=4,default=Decimal(0.00))
+    hostel_due = models.DecimalField(max_digits=10,decimal_places=4,default=Decimal(0.00))
+    academic_due = models.DecimalField(max_digits=10,decimal_places=4,default=Decimal(0.00))
