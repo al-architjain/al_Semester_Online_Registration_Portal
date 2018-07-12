29a375a12359c383b4f5fc789de06e9614f7c8ef
al_architjain <architjain.delhi2@gmail.com>
2018-07-12 20:22:54 +0530

    Static file at one place

--- SORP/SORP/settings.py
+++ SORP/SORP/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 # STATIC_URL = os.path.join(BASE_DIR,'sorp_app','static/')
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'sorp_app','mediafils','sorp_app')
 
